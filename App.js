5b0ec6819c772b3391f875ac91291c8b4860e421
Akshyy31 <akshayshaji688@gmail.com>
2025-06-20 12:15:10 +0530

    React Resto app 20-6-25

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-let heading = React.createElement(
-  "h1",
-  { id: "heading", abc: "abc" },
-  React.createElement(
-    "div",
-    { id: "second" },
 
-    [
-      React.createElement("h1", { id: "h1" }, " Namaste React"),
-      React.createElement("h2", { id: "h2" }, "Hello"),
-    ]
-  )
-);
+// let heading = React.createElement("h1", { id: "heading" }, "Namasthe React ");
+// let root = ReactDOM.createRoot(document.getElementById("main"));
+// root.render(heading);
 
-let root = ReactDOM.createRoot(document.getElementById("main"));
+// jsx
 
-console.log(root);
+// let head = <h1 id="head" className="head"> jsx react element  </h1>
+// let root1 = ReactDOM.createRoot(document.getElementById("main2"));
+// root1.render(head);
 
-root.render(heading);
+// functional component
+// const Title = () => <h2>Title component</h2>;
+
+// function HeadingComponent() {
+//   return (
+//     <div>
+//       {/* <Title/>
+//       {Title()} */}
+//       <h1>Namaste React Functional component</h1>
+//     </div>
+//   );
+// }
+// let ele = (
+//   <>
+//     <HeadingComponent />
+//     <h2>Hello </h2>
+//   </>
+// );
+
+// let root = ReactDOM.createRoot(document.getElementById("main"));
+// root.render(ele);
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo">
+        <img
+          alt="logo-image"
+          src="https://marketplace.canva.com/EAFMme8qKdk/1/0/1600w/canva-modern-restaurant-bar-and-grill-food-logo-qG0JgcwrqHQ.jpg"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>HOME</li>
+          <li>About</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Applayout = () => {
+  return (
+    <div className="appLayout">
+      <Header />
+    </div>
+  );
+};
+
+let root = ReactDOM.createRoot(document.getElementById("appLayout"));
+root.render(<Applayout />);
